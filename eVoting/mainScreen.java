2a4ae3fe65a643b3c76c264bbe0bc900b670725d
sarandford <fordsr@g.cofc.edu>
2016-12-02 10:12:38 -0500

    Driver methods still need to be broken out more, but this breaks up the mainScreen methods a bit

--- eVoting/mainScreen.java
+++ eVoting/mainScreen.java
@@ -26,6 +26,12 @@ public class mainScreen extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField idTextField;
+	private Driver driver;
+	private JPanel title;
+	private JLabel titleLabel;
+	private JPanel body;
+	private JPanel submitButton;
+	
 
 	/**
 	 * Launch the application.
@@ -36,49 +42,39 @@ public class mainScreen extends JFrame {
 				try {
 					mainScreen frame = new mainScreen();
 					frame.setVisible(true);
+					frame.createSignInScreen();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public mainScreen() {
-		Driver driver = new Driver();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
-
-		JLabel lblNewLabel = new JLabel("Welcome to the eVoting System for the State of South Carolina");
-		panel.add(lblNewLabel);
-
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.SOUTH);
-		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-
-		JPanel panel_2 = new JPanel();
-		contentPane.add(panel_2, BorderLayout.CENTER);
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));//TODO only allow one radio button to be selected and select voter by default
-
+	public void transitionScreens(){
+		title.removeAll();
+		body.removeAll();
+	}
+	
+	public void createVoterSelectionScreen(){
+		
+	}
+	
+	public void createPollingOfficialSelectionScreen(){
+		
+	}
+	
+	public void createSignInScreen(){
+		titleLabel.setText("Welcome to the eVoting System for the State of South Carolina");
+		title.add(titleLabel);
 		JRadioButton voterRadioButton = new JRadioButton("I am a voter");
 		voterRadioButton.setSelected(true);
-		panel_2.add(voterRadioButton);
+		body.add(voterRadioButton);
 		voterRadioButton.setVerticalAlignment(SwingConstants.BOTTOM);
 
 		JRadioButton pollingOfficialRadioButton = new JRadioButton("I am a polling official");
-		panel_2.add(pollingOfficialRadioButton);
+		body.add(pollingOfficialRadioButton);
 
 		idTextField = new JTextField("id");
-		panel_2.add(idTextField);
+		body.add(idTextField);
 		idTextField.setColumns(10);
 
 		JButton signInButtonforVoterAndPollingOfficial = new JButton("Sign In");
@@ -91,14 +87,14 @@ public class mainScreen extends JFrame {
 				// TODO add validate input checks
 				if (driver.signInCounter < 3) {
 					int result = driver.validate(idTextField.getText(), "V");
-					if (result == 0) {
+					if (result == 1) {//voter has voted
 						returnLabel.setText("This id is associated with a voter who has already voted");
 						System.out.println("VOTER HAS VOTED");
 						driver.signInCounter--;
 						
-					} else if (result == 1) {
+					} else if (result == 0) {//voter has not voted and is valid
 						returnLabel.setText("VOTER INFO HERE");//TODO get voter info
-						driver.getVoterInfo(idTextField.getText());
+						returnLabel.setText(driver.getVoterInfo(idTextField.getText()));
 					} else {
 						System.out.println("NOT A VALID VOTER");
 						driver.signInCounter--;
@@ -109,8 +105,28 @@ public class mainScreen extends JFrame {
 				}
 			}
 		});
-		panel_2.add(signInButtonforVoterAndPollingOfficial);
-		panel_2.add(returnLabel);
+		body.add(signInButtonforVoterAndPollingOfficial);
+		body.add(returnLabel);
+		
 	}
+	/**
+	 * Create the frame.
+	 */
+	public mainScreen() {
+		this.driver = new Driver();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		this.contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		this.title = new JPanel();
+		contentPane.add(title, BorderLayout.NORTH);
+		this.titleLabel = new JLabel();
+		this.body = new JPanel();
+		contentPane.add(body, BorderLayout.CENTER);
+		body.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));//TODO only allow one radio button to be selected and select voter by default
+	}
+	
 
 }
