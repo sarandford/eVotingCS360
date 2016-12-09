c928d6395c1fcf7afc0602f9bb487457814d9fe5
Hassam Solano <solanomorelhs@g.cofc.edu>
2016-12-09 18:21:30 -0500

    Correct Base64 encoding

--- eVoting/Hashing.java
+++ eVoting/Hashing.java
@@ -1,20 +1,19 @@
 package eVoting; 
 
 import java.security.MessageDigest;
-import sun.misc.BASE64Encoder;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 
 
 public class Hashing {
 	
 	private final String algo = "SHA-256";
 	private  MessageDigest digest;
-	private BASE64Encoder encoder;
 
 	
 	public Hashing(){
 		try{
 			this.digest = MessageDigest.getInstance(algo);
-			this.encoder = new BASE64Encoder();
 			
 		}catch (Exception e){
 			System.out.println(e.getMessage());
@@ -28,7 +27,7 @@ public class Hashing {
 		digest.update(value.getBytes());
 		byte[] valueBytes = digest.digest();
 		
-		hash = encoder.encode(valueBytes);
+		hash = Base64.getEncoder().encodeToString(valueBytes);
 		
 		digest.reset();
 				
